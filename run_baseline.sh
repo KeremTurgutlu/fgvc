f8b58ba52282cf75a6fb32473e773ced63185fb6
KeremTurgutlu <keremturgutlu@gmail.com>
2021-03-21 10:00:26 +0000

    typo

--- run_baseline.sh
+++ run_baseline.sh
@@ -1,4 +1,4 @@
 python train_plant_baseline.py --arch tf_efficientnet_b4_ns --size 448 --lr 2e-2 --bs 32 --epochs 24
 python train_plant_baseline.py --arch resnet101d            --size 448 --lr 2e-2 --bs 32 --epochs 24
 python train_plant_baseline.py --arch vit_base_patch16_384  --size 384 --lr 1e-3 --bs 32 --epochs 24
-python train_plant_baseline.py --arch vit_base_resnet50_384 --size 284 --lr 1e-3 --bs 24 --epochs 24
+python train_plant_baseline.py --arch vit_base_resnet50_384 --size 384 --lr 1e-3 --bs 24 --epochs 24
